b41eff88ee8c8a6c8c27423a80c806e5931607ac
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-29 18:00:39 -0300

    fix: Imagens, favico e previewIcon

--- src/components/interactives/WhatsAppAnimated.jsx
+++ src/components/interactives/WhatsAppAnimated.jsx
@@ -88,7 +88,7 @@ export default function WhatsappAnimated() {
   const themes = whatsAppColor ? whatsAppThemes : defaultButtonThemes
 
   const colorButton = themes[colorMode] || themes.default
-  const iconColor = colorMode === 'light' ? '#fff' : '#fff'
+  const iconColor = colorMode === 'light' ? '#fff' : '#000'
 
   console.log({
     whatsAppColor,
