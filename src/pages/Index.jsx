db9c5508b0a68df722763e98c92c2d5a2e4feb95
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-16 18:23:40 -0300

    fix: Modal about

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -94,7 +94,7 @@ export default function Index() {
             <Suspense>
               <AboutNovoTemplate
                 colorMode={colorMode}
-                ButtonModal={false}
+                ButtonModal={true}
                 benefits={true}
               />
               <SocialMediaTemplate colorMode={colorMode} />
