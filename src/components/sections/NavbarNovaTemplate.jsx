3ab9c450f6666100ce6cfe28f3fb5a709f2946df
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-16 09:44:03 -0300

    fix: Logo speed

--- src/components/sections/NavbarNovaTemplate.jsx
+++ src/components/sections/NavbarNovaTemplate.jsx
@@ -102,7 +102,7 @@ function NavbarNovaTemplate({
                 alt={content.texts.navbar.logo.alt}
                 className="desktop1:hidden w-fit max-h-full object-contain flex"
                 width={187}
-                height={119}
+                height={129}
                 fetchPriority="high"
               />
               <img
