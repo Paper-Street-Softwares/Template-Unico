08826793d90e404b8a6aac4a9374dfba4b90c0b4
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-17 14:39:22 -0300

    fix: Infos

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -39,14 +39,14 @@ import { FileText } from "lucide-react";
 const currentYear = new Date().getFullYear();
 
 export const infos = {
-  name: "A_Definir",
+  name: "Tavares Assessoria Jurídica",
   email: "contato", // Email desejado pelo cliente
   emailSecundario: "A_Definir", // Email desejado pelo cliente
-  domain: "A_Definir", // Sem "www"
+  domain: "condominial.taj.adv.br", // Sem "www"
   phone: {
-    ddd: "00",
-    firstPart: "00000", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "0000", // Apenas os 4 últimos números
+    ddd: "11",
+    firstPart: "97394", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "0876", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "00",
@@ -61,10 +61,10 @@ export const infos = {
   endereco: (
     <div>
       {/* <p>Endereço:</p> */}
-      <p>A_Definir,</p>
-      <p>A_Definir,</p>
-      <p>A_Definir,</p>
-      <p>A_Definir,</p>
+      <p>Av. Industrial 780,</p>
+      <p>Sala 2313 - Jardim, </p>
+      <p>Santo André - SP,</p>
+      <p>CEP: 09080-500</p>
     </div>
   ),
   enderecoSecundario: (
