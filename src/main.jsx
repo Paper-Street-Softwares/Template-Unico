c68aa146af2f8a127c60c8300a2a896f4595097f
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-17 08:18:17 -0300

    fix: Realoca o import do css

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import './styles/utilities.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./styles/utilities.css";
+import "./index.css";
 
-import Index from './pages/Index.jsx'
+import Index from "./pages/Index.jsx";
 
-import { useColorMode } from './context/UseContextArchive.jsx'
-import { ColorModeProvider } from './context/UseContextArchive.jsx'
-import GlobalClickEvent from './context/GlobalClickEvent.jsx'
+import { useColorMode } from "./context/UseContextArchive.jsx";
+import { ColorModeProvider } from "./context/UseContextArchive.jsx";
+import GlobalClickEvent from "./context/GlobalClickEvent.jsx";
 
 // Altere aqui para "LP" ou "site"
-const mode = 'LP' // só muda isso e o resto se adapta
+const mode = "LP"; // só muda isso e o resto se adapta
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <ColorModeProvider>
-    <GlobalClickEvent />{' '}
+    <GlobalClickEvent />{" "}
     <Router>
       <Routes>
         <Route path="/" element={<Index mode={mode} />} />
       </Routes>
     </Router>
   </ColorModeProvider>,
-)
+);
