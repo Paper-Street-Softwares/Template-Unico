2e175ced6e19dae9e3d82553590bb67d0b082d15
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-17 08:33:13 -0300

    fix: Layoult

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import GlobalClickEvent from "./context/GlobalClickEvent.jsx";
 
 // Altere aqui para "LP" ou "site"
 const mode = "LP"; // só muda isso e o resto se adapta
+window.addEventListener("load", () => {
+  document.body.style.opacity = "1";
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ColorModeProvider>
